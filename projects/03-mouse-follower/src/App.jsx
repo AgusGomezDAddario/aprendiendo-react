ea20295a974404d3c0848272a9dd3095deb6844c
AgusGomezDAddario <gomezdaddarioagus@gmail.com>
2024-07-16 22:19:30 -0300

    Primeros avances en la aplicacion 3

---
+++ projects/03-mouse-follower/src/App.jsx
@@ -0,0 +1,39 @@
+import './App.css'
+import { useEffect, useState } from 'react'
+function App() {
+  const [enabled, setEnabled] = useState(false)
+
+  useEffect(() => {
+    console.log('effect', {enabled})
+    
+    const handleMove = (event) => {
+      const { clientX, clientY } = event
+      console.log('handleMove', {clientX, clientY})
+    }
+    window.addEventListener('pointermove', handleMove)
+    }, [enabled])
+
+  return (
+    <main>
+      <div style={{
+        position: 'absolute',
+        backgroundColor: 'rgba(0, 0, 0, 0.5)',
+        border: '1px solid #fff',
+        borderRadius: '50%',
+        opacity: 0.8,
+        pointerEvents: 'none',
+        left: -20,
+        top: -20,
+        width: 40,
+        height: 40,
+        transform: 'translate(0px, 0px)'
+      }}
+      />
+      <button onClick={() => setEnabled(!enabled)}>
+        {enabled ? 'Desactivar' : 'Activar'} seguir puntero
+      </button>
+    </main>
+  )
+}
+
+export default App

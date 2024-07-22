2115f2d3d333ed1f8a9f05f01f67b0c74dac50e4
Agustín Gomez D'Addario <gomezdaddarioagus@gmail.com>
2024-07-21 22:25:55 -0300

    Cambios en la presentacion de las imagenes

--- projects/05-react-prueba-tecnica/src/App.jsx
+++ projects/05-react-prueba-tecnica/src/App.jsx
@@ -1,28 +1,44 @@
 import React, { useEffect, useState } from 'react';
+import './App.css';
 
 const CAT_ENDPOINT_RANDOM_FACT = 'https://catfact.ninja/fact';
+const CAT_ENDPOINT_IMAGE_URL = 'https://cataas.com/cat/says/';
+const CAT_ENDPOINT_IMAGE_PARAMS = '?size=10&color=red';
+
 export function App (){
     const [fact, setFact] = useState();
     const [imageUrl, setImageUrl] = useState();
+    const [factError, setFactError] = useState();
 
+    // Primer efecto para obtener un random fact
     useEffect(() => {
         fetch(CAT_ENDPOINT_RANDOM_FACT)
-            .then(res => res.json())
+            .then(res => {
+                if (!res.ok) {
+                    setFactError('Error al obtener el fact')
+                }
+                 return res.json()
+            })
             .then(data => {
                 const {fact} = data;
                 setFact(fact);
+            })
+    }, []);
 
-                const firstWord = fact.split(' ')[0];
+    // Segundo efecto para obtener la imagen del gato cada vez que cambia el fact
+    useEffect(() => {
+        if (!fact) return
+        const firstWord = fact.split(' ')[0];
 
-                fetch(`https://cataas.com/cat/says/${firstWord}?size=20&color=red`)
+                fetch(`${CAT_ENDPOINT_IMAGE_URL}${firstWord}${CAT_ENDPOINT_IMAGE_PARAMS}`)
                     // .then(res => res.json())
                     .then(response => {
                         const { url } = response
                         console.log(url)
                         setImageUrl(url)
                     })
-            })
-    }, []);
+    }, [fact]);
+
 
     return(
         <main>

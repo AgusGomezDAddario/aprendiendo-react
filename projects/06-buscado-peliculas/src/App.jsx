bea4ce1bb21ee0af282d842a666130f9945f3a87
Agustín Gomez D'Addario <gomezdaddarioagus@gmail.com>
2024-07-24 15:43:01 -0300

    Primeros avances con buscador de peliculas

--- projects/06-buscado-peliculas/src/App.jsx
+++ projects/06-buscado-peliculas/src/App.jsx
@@ -1,19 +1,31 @@
 import './App.css'
+import { useMovies } from './hooks/useMovies'
+import { Movies } from './components/Movie.jsx'
+import { useRef } from 'react' // Permite almacenar valores que persisten entre renderizados
 
 function App() {
+
+  const {movies: mappedMovies} = useMovies()
+  const inputRef = useRef() // Almacena el valor del input
+
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    const value = inputRef.current.value
+    console.log(value)
+  }
 
   return (
     <div className='page'>
       <header>
         <h1>Buscador de películas</h1>
-        <form className='form'>
-          <input placeholder='Avengers, Star Wars, The Matrix ...' />
+        <form className='form' onSubmit={handleSubmit}>
+          <input ref={inputRef} placeholder='Avengers, Star Wars, The Matrix ...' />
           <button type='submit'>Buscar</button>
         </form>
       </header>
 
       <main>
-        Resultados de las películas
+        <Movies movies={mappedMovies}/>
       </main>
     </div>
   )
